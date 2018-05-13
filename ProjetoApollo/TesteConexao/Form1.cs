e971ff315ed4938f4ec7bd71dd746c86b766d4ae
William Roger <williamroger.frontend@outlook.com>
2018-05-12 23:01:54 -0300

    Adicionada Classe de Conexão e um Teste de Conexão

---
+++ ProjetoApollo/TesteConexao/Form1.cs
@@ -0,0 +1,35 @@
+﻿using Biblioteca.pacoteConexao;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesteConexao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTestarConexao_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ConexaoSQLServer conexao = new ConexaoSQLServer();
+                conexao.abrirConexao();
+                MessageBox.Show("A Conexao tá rolando malandro!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
