04b8dd109f47f956399f070d73c4d88c415c6093
William Roger <williamroger.frontend@outlook.com>
2018-06-04 09:16:10 -0300

    Adicionado ArtistaLocacao (Falta implementar a parte de Negocio)

---
+++ ProjetoApollo/Biblioteca/pacoteArtistaLocacao/ArtistaLocacao.cs
@@ -0,0 +1,48 @@
+﻿using Biblioteca.pacoteArtista;
+using Biblioteca.pacoteLocacao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.pacoteArtistaLocacao
+{
+    [DataContract]
+    class ArtistaLocacao
+    {
+        private int quantidade;
+        private double valor;
+        private Artista artistaID;
+        private Locacao locacaoID;
+
+        [DataMember(IsRequired = true)]
+        public int Quantidade
+        {
+            get { return quantidade;  }
+            set { quantidade = value; }
+        }
+
+        [DataMember(IsRequired = true)]
+        public double Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        [DataMember(IsRequired = true)]
+        public Artista ArtistaID
+        {
+            get { return artistaID; }
+            set { artistaID = value; }
+        }
+
+        [DataMember(IsRequired = true)]
+        public Locacao LocacaoID
+        {
+            get { return locacaoID; }
+            set { locacaoID = value; }
+        }
+    }
+}
---
+++ ProjetoApollo/Biblioteca/pacoteArtistaLocacao/ArtistaLocacaoNegocio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.pacoteArtistaLocacao
+{
+    class ArtistaLocacaoNegocio: InterfaceArtistaLocacao
+    {
+        public void InserirArtistaLocacao(ArtistaLocacao artistaLocacao)
+        {
+
+        }
+
+        public void DeletarArtistaLocacao(ArtistaLocacao artistaLocacao)
+        {
+
+        }
+
+        public void AlterarArtistaLocacao(ArtistaLocacao artistaLocacao)
+        {
+
+        }
+
+        public List<ArtistaLocacao> ListarArtistaLocacao(ArtistaLocacao filtro)
+        {
+
+        }
+    }
+}
